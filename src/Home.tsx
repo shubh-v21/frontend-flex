6fcc71e4f27eb85ef01808f835fe282cfc921ede
Shubh Verma <shubhverma2003@gmail.com>
2025-05-30 01:29:37 +0530

    feat: implement Home component layout with sections

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,21 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import AboutSection from "./components/AboutSection";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import ProjectSection from "./components/ProjectSection";
 
-export default Home
+const Home = () => {
+  // Create more sample projects to demonstrate pagination
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <HeroSection />
+      <ProjectSection />
+      <AboutSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
