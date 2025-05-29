b385fb53b569810f6d43d4098f26e1557b6fff49
Shubh Verma <shubhverma2003@gmail.com>
2025-05-30 01:28:39 +0530

    feat: add allProjects data structure with initial project details

---
+++ src/data/projects.ts
@@ -0,0 +1,18 @@
+import todo from "../assets/project_icons/todo.png";
+
+interface Project {
+  title: string;
+  description: string;
+  techStack: string[];
+  imageUrl: string;
+}
+
+export const allProjects: Project[] = [
+  {
+    title: "React Todo App",
+    description:
+      "A simple but powerful todo application with filter and search capabilities.",
+    techStack: ["React", "TypeScript", "TailwindCSS"],
+    imageUrl: todo,
+  },
+];
